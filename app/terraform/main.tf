d3e1aebc507ca7c8fb46cc1c01821b34f141e47c
Alex <aosilvester@gmail.com>
2025-12-17 12:46:09 -0500

    add base terraform configuration

---
+++ app/terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3 = "http://localstack:4566"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-local-bucket"
+}
