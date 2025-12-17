b2b8ae82cec75bc0caf5a19a55a83a464384c0cc
Alex <aosilvester@gmail.com>
2025-12-17 13:11:13 -0500

    add lambda base code and terraform config infrastructure

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -15,10 +15,33 @@ provider "aws" {
   s3_use_path_style           = true
 
   endpoints {
-    s3 = "http://localstack:4566"
+    s3      = "http://localstack:4566"
+    lambda  = "http://localstack:4566"
   }
 }
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-local-bucket"
+}
+
+
+# Automatically create function.zip
+resource "null_resource" "zip_lambda" {
+  triggers = {
+    lambda_src = filemd5("../lambda/my_lambda/lambda_function.py")
+  }
+
+  provisioner "local-exec" {
+    command = "cd ../lambda/my_lambda && zip -r function.zip lambda_function.py"
+  }
+}
+
+# Lambda function
+resource "aws_lambda_function" "my_lambda" {
+  depends_on    = [null_resource.zip_lambda]   # ensure zip is created first
+  function_name = "my-local-lambda"
+  filename      = "../lambda/my_lambda/function.zip"
+  handler       = "lambda_function.handler"
+  runtime       = "python3.10"
+  role          = "arn:aws:iam::000000000000:role/lambda-ex"
 }
